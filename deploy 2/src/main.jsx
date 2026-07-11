0cc6539a6bbdbfa72b09808eaf052fd2cf6dcae7
privi-doughnut <its.the.prithivi.show@gmail.com>
2026-07-11 15:30:44 -0400

    Delete deploy 2 directory

--- deploy 2/src/main.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import RuneScript from './App.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <RuneScript />
-)
